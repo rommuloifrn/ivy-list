93f7079fabfd183c9048f29df4deb98a940cd446
rommuloifrn <roomulo.cavalkant@outlook.com>
2023-07-31 19:22:34 -0300

    first

---
+++ base/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=40)
+    description = models.TextField(max_length=300, null=True, blank=True)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Meta:
+    ordering = ['complete']
